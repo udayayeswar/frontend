d5499735083813c3e5fb4e128285514d7f85af92
udaya <udayeswari.savana@gmail.com>
2022-03-24 15:34:35 -0400

    module added

--- mutable/main.tf
+++ mutable/main.tf
@@ -1,6 +1,6 @@
 module "ec2" {
   source = "git::https://github.com/udayayeswar/terraform-mutable-ec2.git"
-  SERVER COUNT = var.SERVER_COUNT
+  SERVER_COUNT = var.SERVER_COUNT
 }
 
 #module "alb" {
